051a7a6c969f1dafd5797f64bca49f29bdd2f8b9
SungJunKang <std07209@naver.com>
2018-01-20 16:25:25 +0900

    googleMap

--- app/src/main/java/com/keen/android/happckathon/ViewList.java
+++ app/src/main/java/com/keen/android/happckathon/ViewList.java
@@ -11,35 +11,10 @@ public class ViewList {
     public String text;
 
 
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
+    public ViewList(String title, String date, String image, String text){
         this.title = title;
-    }
-
-    public String getDate() {
-        return date;
-    }
-
-    public void setDate(String date) {
         this.date = date;
-    }
-
-    public String getImage() {
-        return image;
-    }
-
-    public void setImage(String image) {
         this.image = image;
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    public void setText(String text) {
         this.text = text;
     }
 }
